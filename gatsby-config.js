6dd0cc763af3e2f00ff2da11991e85b146074304
The Mage <themage@Thes-MacBook-Pro.local>
2020-03-04 11:33:22 -0600

    Commit message

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
         basePath: "/",
         authorsPage: true,
         sources: {
-          //local: true,
+          local: true,
           contentful: true,
         },
       },
